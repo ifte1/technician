586c334d00f6bb6d58b1defea2aa839fb9dfaf68
Saqib <saqib29111@gmail.com>
2020-03-18 03:29:37 +0600

    created Home Page

---
+++ HomePage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/styleForHomePage.css">
+</head>
+<body>
+    <header>
+        <div class="main">
+            <ul class="left">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Catagory</a></li>
+                <li><a href="#">Technician</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+            <ul class="right">
+                <li><a href="#">Ragistration</a></li>
+                <li><a href="#">Log in</a></li>
+            </ul>
+        </div>
+        <h1 class="t">Technician Finder</h1>
+    </header>
+    <div class="back">
+        <h1>Our Service:</h1>
+        <div class="services">
+            <div class="a"><h5>Electrical Technician</h5></div>
+            <div class="a"><h5>Car <br>Technician</h5></div>
+            <div class="a"><h5>Computer Technician</h5></div>
+            <div class="a"><h5>Industrial Technician</h5></div>
+            <div class="a"><h5>Plumber Technician</h5></div>
+            <div class="a"><h5>Rediological Technician</h5></div>
+        </div>
+    </div>
+    
+</body>
+</html>
