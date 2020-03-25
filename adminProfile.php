a1da9408ce9e92e115a8dc09a323277109738465
Saqib <saqib29111@gmail.com>
2020-03-25 22:06:01 +0600

    new folder for images, adminProfile and styleFor Admin has created

---
+++ adminProfile.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Page</title>
+    <link rel="stylesheet" href="css/styleForAdmin.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="cover_profile">
+            <div class="cover">
+                <div class="menu_bar">
+                   <ul class="options">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Catagory</a></li>
+                    <li><a href="#">Technician</a></li>
+                    <li><a href="#">About</a></li>
+                   </ul>
+               </div>
+               <div class="profile">
+                
+               </div>
+            </div>
+            
+        </div>
+    </div>
+    
+    
+</body>
+</html>
