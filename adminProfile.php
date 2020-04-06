aa9fb44b6892b43670414c6be020adf695089506
Saqib <saqib29111@gmail.com>
2020-04-06 22:48:50 +0600

    adminProfile.php and styleForAdmin.css are completed

--- adminProfile.php
+++ adminProfile.php
@@ -12,25 +12,43 @@
             <div class="cover">
                 <div class="menu_bar">
                    <ul class="options">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Catagory</a></li>
+                    <li><a href="#">User</a></li>
                     <li><a href="#">Technician</a></li>
-                    <li><a href="#">About</a></li>
                    </ul>
                    <ul id="left-option">
-                      <li><a href="#">Search</a></li>
                        <li><a href="#">Log out</a></li>
                    </ul>
                </div>
-               <div class="profile">
+               <div class="profile1">
                 
                </div>
+               <div class="profile2">
+                   
+               </div>
+               <div class="profile3">
+                   
+               </div>
             </div>
-            
-        
         </div>
     </div>
     
-    
+    <div class="box1">
+        <span>Id: 17-34832-2</span><br><br>
+        <span>Name: Ifte Khairul Islam</span><br><br>
+        <span>Mobile No: 01792348322</span><br><br>
+        <span>Address: Nikunj, DHaka</span><br><br>
+    </div>
+    <div class="box2">
+        <span>Id: 17-34879-2</span><br><br>
+        <span>Name: Saqib Aminul islam</span><br><br>
+        <span>Mobile No: 01821500050</span><br><br>
+        <span>Address: Chashara, Narayanganj</span><br><br>
+    </div>
+    <div class="box3">
+        <span>Id: 17-34935-2</span><br><br>
+        <span>Name: Hasin Ashraf rana</span><br><br>
+        <span>Mobile No: 01627188454</span><br><br>
+        <span>Address: Cantonment, Dhaka</span><br><br>
+    </div>
 </body>
 </html>
