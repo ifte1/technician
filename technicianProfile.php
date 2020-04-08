a498d683ce119499d26e39db181f6150be1923c8
Saqib <saqib29111@gmail.com>
2020-04-08 18:59:04 +0600

    technician page and stylesheet for this page hase benn formed and modified the name Technicina.php to technician.php

---
+++ technicianProfile.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>technician</title>
+    <link rel="stylesheet" href="css/styleForTechnicianProfile.css">
+</head>
+<body>
+    <div class="container">
+        <div class="cover_profile">
+            <div class="cover">
+                <div class="menu_bar">
+                   <ul class="options">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Catagories</a></li>
+                    <li><a href="#">Technician</a></li>
+                    <li><a href="#">About</a></li>
+                   </ul>
+                   <ul id="left-option">
+                       <li><a href="#">Log out</a></li>
+                   </ul>
+               </div>
+               <div class="profile">
+                
+               </div>
+               
+            </div>
+        </div>
+    </div>
+    <div class="box">
+        <div class="spa">
+        <span>Id: 17-0000-2</span><br><br>
+        <span>Name: Soyod ul Amin</span><br><br>
+        <span>Mobile: +8801254698</span><br><br>
+        <span>Address: bashabo, Dhaka</span><br><br>
+        <span>Feedback: ****</span><br><br>
+        </div>
+    </div>
+</body>
+</html>
