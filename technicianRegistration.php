27046ee2b53aa33a74d2f09fa2701a73489e180d
ifte1 <iftekharislam111@gmail.com>
2020-04-09 02:28:47 +0600

    image option added

--- technicianRegistration.php
+++ technicianRegistration.php
@@ -26,39 +26,41 @@
                 <div id="btn"></div>
                 <button type="button" class="toggle-btn">Technician</button>
             </div>
-            <form id="technician" class="input-group"action="#" onsubmit="return validation();">
-                <input type="text" class="input-field" placeholder="Full Name" required>
+            <form id="technician" class="input-group" action="Back_End/tTechnicianRegCheck.php" onsubmit="return validation();" method="post" enctype="multipart/form-data">
+                <input type="text" class="input-field" placeholder="Full Name" name="fullname"required>
 
-                <input type="text" class="input-field" placeholder="User Name" id="username">
+                <input type="text" class="input-field" placeholder="User Name" name="username" id="username" >
                 <label id="lc" style=" color:red; visibility:hidden;">* Minimum 3 letters </label>
 
-                <input type="phone" class="input-field" placeholder="Mobile Number" id="mobile" >
-                <label id="mlc" style=" color:red; visibility:hidden;">* Enter a valid mobile number</label><br>
+                <input type="phone" class="input-field" placeholder="Mobile Number" name="mobile"id="mobile" >
+                <label id="mlc" style=" color:red; visibility:hidden;">* Enter a valid mobile number</label></br>
                 
                 Sex:                  
                 <input type="radio" class="radio-field" name="gender" value="Female" required>Female
                 <input type="radio" name="gender" class="radio-field" value="Male" required>Male
                 <input type="radio" name="gender" class="radio-field" value="Other" required>Other
                 
-                <input type="password" class="input-field" placeholder="Password" id="password" >
+                <input type="password" class="input-field" placeholder="Password" id="password" name="password" >
                 <label id="plc" style=" color:red; visibility:hidden;">* password length at least 6</label>
 
                 <input type="password" class="input-field" placeholder="Confirm Password" id="cpassword">
                 <label id="clc" style=" color:red; visibility:hidden;"> * Password and confirm password must be same</label>
                 
-                <input type="email" class="input-field" placeholder="Email" id=email>
+                <input type="email" class="input-field" placeholder="Email" id="email" name="email">
                 <label id="elc" style=" color:red; visibility:hidden;">* Enter a valid email </label>
-                <input type="date" class="input-field" placeholder="Date of birth" required>
+                <input type="date" class="input-field" placeholder="Date of birth" name="birth" required>
                 <br><br>
+                </select>
                 <span>Experience:</span>
-                <select class="cat" id="technicians" name="catagory" form="technician"required>
+                <select class="cat" id="technicians" name="experience" form="technician"required>
                       <option value="low" >1-5 years</option>
                       <option value="medium" >5-10 years</option>
                       <option value="high" >10-15 years</option>
                       <option value="very high">>15</option>
                   
+                  
                 </select>
-                <br><br>
+                <br>
                 <span>Catagory:</span>
                 <select class="cat" id="technicians" name="catagory" form="technician" required>
                       <option value="Ethical Technician">Ethical Technician</option>
@@ -67,19 +69,24 @@
                       <option value="Industrial Technician">Industrial Technician</option>
                       <option value="Radiological Technician">Radiological Technician</option>
                       <option value="Plumber">Plumber</option>
-                </select>
+                  
                 
                 
-                <input type="text" class="input-field" placeholder="Address" required>
+                <input type="text" class="input-field" placeholder="Address" name="address" required>
+       
+                <input type="file" class="input-field"  name="file" id="file"  required>
                 
-                <button type="button" class="submit-btn">Home</button>
-                <button type="submit" class="submit-btn">Register</button>
+                <button type="button" class="submit-btn"> <li><a href="HomePage.php">Home</a></li> </button>
+                <button type="submit" name="submit"id="submit" value='submit'class="submit-btn">Register</button>
             </form>
             
         </div>
     </div> 
 
-    <script src="js/technicianValid.js"></script>
+    <script src="js/technicianValid.js">
+  
+
+</script>
 
 
 </body>
