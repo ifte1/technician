68ecc2c4b5541132441536cc7f9b97ae3024b1ed
ifte1 <iftekharislam111@gmail.com>
2020-04-09 12:15:55 +0600

    reject technician is done

---
+++ Back_End/rejectTechnician.php
@@ -0,0 +1,15 @@
+<?php
+include('../dbControl/db.php');
+
+$connection = new db();
+$conobj=$connection->OpenCon();
+
+$id = $_GET['id'];
+
+$q = "DELETE FROM `tTechnician` WHERE tTechnician.`id` = '$id' ";
+
+mysqli_query($conobj,$q);
+
+header('location:tTechnicianTable.php');
+
+?>
