5cb73abb7d8ed0b8fd23e89ef65e1ab6234fe805
Saqib <saqib29111@gmail.com>
2020-04-14 18:13:04 +0600

    messagetechnican.php applied css

--- Back_End/messageTechnician.php
+++ Back_End/messageTechnician.php
@@ -5,23 +5,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+    <link rel="stylesheet" href="../css/StyleFormessagetechnician.css">
 </head>
 <body>
-    
-<form id="user" class="input-group" action="" method="post">
-         To:
+
+<div class="container">
+
+  <form id="user" class="input-group" action="" method="post">
+         <h4>To:</h4>
         <input type="text" class="input-field" placeholder="User Name" name="username">
           <br><br>
-          Message:                     
-        <input type="text" class="input-field" placeholder="message" name="message" >
+          <h4>Message:</h4>                     
+        <textarea name="textarea" id="message-field" cols="40" rows="5" placeholder="Write Message..."></textarea>
          <br> <br>
         <button type="submit" name="submit" class="submit-btn">Send</button>
-
+        <button type="button" name="cancel" class="submit-btn">Cancel</button>
         <?php 
- include'messagepass.php';
+          include'messagepass.php';
 
-?>
+        ?>
+  </form>
+  
+</div>    
+
 
 </body>
 </html>
