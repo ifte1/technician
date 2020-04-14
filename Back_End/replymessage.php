2f99586787ec517d37d903044362e1184414eaba
Saqib <saqib29111@gmail.com>
2020-04-14 19:20:54 +0600

    replymessage.php and css file created and modified

--- Back_End/replymessage.php
+++ Back_End/replymessage.php
@@ -5,20 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+    <link rel="stylesheet" href="../css/StyleForreplymessage.css">
 </head>
 <body>
     
-<form id="user" class="input-group" action="" method="post">
-          Message:                     
-        <input type="text" class="input-field" placeholder="message" name="message" >
+    <div class="container">
+    <form id="user" class="input-group" action="" method="post">
+        <h4>Message:</h4>                  
+        <textarea name="textarea" id="message-field" cols="40" rows="5" placeholder="Write Message..."></textarea>
          <br> <br>
         <button type="submit" name="submit" class="submit-btn">Send</button>
 
         <?php 
- include'replypass.php';
+          include'replypass.php';
 
-?>
+        ?>
+    </from>
+    
+    </div>
 
 </body>
 </html>
