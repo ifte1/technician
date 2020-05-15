14c40f4ee8939f242d48d6ce332578087d05b278
Saqib <saqib29111@gmail.com>
2020-05-15 14:24:00 +0600

    new pic added of mine named saqib.jpg and areplymessage.php page added style

--- Back_End/Areplymessage.php
+++ Back_End/Areplymessage.php
@@ -5,20 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+    <link rel="stylesheet" href="../css/areplymessage.css">
 </head>
 <body>
     
-<form id="user" class="input-group" action="" method="post">
+<div class="box">
+    <form id="user" class="input-group" action="" method="post">
           Message:                     
         <input type="text" class="input-field" placeholder="message" name="message" >
          <br> <br>
         <button type="submit" name="submit" class="submit-btn">Send</button>
-
+    
         <?php 
- include'Areplypass.php';
+            include'Areplypass.php';
 
-?>
+        ?>
+        </form>
+</div>
 
 </body>
 </html>
